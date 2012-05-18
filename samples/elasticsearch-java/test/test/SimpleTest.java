2d82e0f5f71a5292f7e3f70fe4d6c0aea3c02482
Nicolas Boire <nboire@gmail.com>
2012-05-18 20:36:10 +0200

    Add Test Sample + travis conf

---
+++ samples/elasticsearch-java/test/test/SimpleTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.*;
+
+import play.mvc.*;
+import play.test.*;
+import play.libs.F.*;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleCheck() {
+        int a = 1 + 1;
+        assertThat(a).isEqualTo(2);
+    }
+}
