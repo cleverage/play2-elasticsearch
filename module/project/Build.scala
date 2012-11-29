d28b4e9c83e2e8f74bb0b98d5fe7fae9eaefcf99
Nicolas Boire <nboire@gmail.com>
2012-11-29 10:22:25 +0100

    #7 : fix compatibility with play2.1.RC1

--- module/project/Build.scala
+++ module/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "elasticsearch"
-    val appVersion      = "0.4.2"
+    val appVersion      = "0.4.3"
 
     val appDependencies = Seq(
       // Add your project dependencies here
